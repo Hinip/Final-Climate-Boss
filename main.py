a545531f7ecec88014d0bf0314023145a79509e1
Hinip <matiprohinojosa@gmail.com>
2025-12-07 12:38:40 -0300

    Añadido Ruletas de Q&A y página intermedia

--- main.py
+++ main.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def menu():
     return render_template("menu/menu.html")
 
+@app.route("/middle")
+def middle():
+    return render_template("middle/middle.html")
+
+@app.route("/roulette")
+def roulette():
+    return render_template("roulette/roulette.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
